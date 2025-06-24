4a192b43c1adbbff8e0e902d9366eccb06bec5d6
satyamdevv27 <satyam.verma.og27@gmail.com>
2025-06-24 22:05:41 +0530

    day 1 finished

--- src/page/product.jsx
+++ src/page/product.jsx
@@ -1,11 +1,50 @@
+import { useEffect, useState } from "react";
+import "../styles/product.css";
 
+function Products() {
+  const [categories, setcategories] = useState([]);
+  const [productsbycategory, setproductsbycategory] = useState({});
 
-function Product() {
-  return (
-    <div>
-      <h1>i am from Product section</h1>
-    </div>
-  )
+  //to fetch all the categories
+  const allproducts = async () => {
+    const response = await fetch(
+      "https://fakestoreapi.com/products/categories"
+    );
+    const categorycata = await response.json();
+    setcategories(categorycata);
+ 
+
+    //to fetch all products of the category
+    const categoryproducts = await Promise.all(
+      categorycata.map(async (category) => {
+        const productsresponse = await fetch(
+          `https://fakestoreapi.com/products/category/${category}`
+        );
+        const productdata = await productsresponse.json();
+        return { category, productdata };
+      })
+    );
+    // to add organise all products according to category in an object 
+    const organizedproducts = categoryproducts.reduce((acc , item)=>{
+      acc[item.category] = item.productdata
+      console.log(acc);
+      return acc;
+      
+    }
+  ,{})
+  setproductsbycategory(organizedproducts)
+
+  };
+// to load the page if tere is any change 
+  useEffect(() => {
+    allproducts();
+  }, []);
+
+  return <>
+  <div className="products_cotainer">
+
+  </div>
+  </>;
 }
 
-export default Product
+export default Products;
