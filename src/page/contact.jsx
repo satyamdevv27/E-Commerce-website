a5a5837e09f601c529842bf29c4a27d3f2cb0e10
satyamdevv27 <satyam.verma.og27@gmail.com>
2025-06-25 21:20:31 +0530

    done

--- src/page/contact.jsx
+++ src/page/contact.jsx
@@ -1,9 +1,17 @@
-
-
+import contact from "../assets/images/contact.webp"
+import "../styles/Contact.css"
 function Contact() {
   return (
-    <div>
-      <h1>i am from Contact section</h1>
+    <div className="contact_sec">
+      <div className="img">
+        <img src={contact} alt="" />
+      </div>
+      <form action="">
+      <input type="text" placeholder="full name" />
+      <input type="email" placeholder="Email" />
+      <textarea name="" placeholder="enter messege"></textarea>
+      <button>send</button>
+      </form>
     </div>
   )
 }
