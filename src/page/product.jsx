ad56e30802615d40b546b7d9b2d2bd06f43edb7a
satyamdevv27 <satyam.verma.og27@gmail.com>
2025-06-25 07:56:43 +0530

    products are displayed on products page

--- src/page/product.jsx
+++ src/page/product.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "../styles/product.css";
-
+import "../styles/product.css"
 function Products() {
   const [categories, setcategories] = useState([]);
   const [productsbycategory, setproductsbycategory] = useState({});
@@ -12,7 +12,6 @@ function Products() {
     );
     const categorycata = await response.json();
     setcategories(categorycata);
- 
 
     //to fetch all products of the category
     const categoryproducts = await Promise.all(
@@ -23,28 +22,49 @@ function Products() {
         const productdata = await productsresponse.json();
         return { category, productdata };
       })
+      // to add organise all products according to category in an object
     );
-    // to add organise all products according to category in an object 
-    const organizedproducts = categoryproducts.reduce((acc , item)=>{
-      acc[item.category] = item.productdata
-      console.log(acc);
-      return acc;
-      
-    }
-  ,{})
-  setproductsbycategory(organizedproducts)
+    const organizedproducts = categoryproducts.reduce((acc, item) => {
+      acc[item.category] = item.productdata;
+console.log(acc);
 
+      return acc;
+    }, {});
+
+    setproductsbycategory(organizedproducts);
+    
+   
+    
   };
-// to load the page if tere is any change 
+  // to load the page if tere is any change
   useEffect(() => {
     allproducts();
   }, []);
 
-  return <>
-  <div className="products_cotainer">
-
-  </div>
-  </>;
+  return (
+    <>
+      <div className="products_cotainer">
+        {Object.entries(productsbycategory).map(([category, products]) => (
+          <div className="product_category" key={category}>
+            <h2>{category.toLocaleUpperCase()}</h2>
+            <div className="product_list">
+              {
+                products.map((product)=>(
+                  <div className="product_card" key={product.id}>
+                     <img src={product.image} alt="" />
+                     <h3>{product.title}</h3>
+                     <p>price : ₹{product.price}</p>
+                     <button>buy now</button>
+                  </div>
+                ))
+              }
+              
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Products;
